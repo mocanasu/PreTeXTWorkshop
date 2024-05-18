9f027133f22f3bf10fe9af65af6f257d31715705
Mona Mocanasu <mmocanas@msudenver.edu>
2024-05-18 02:46:16 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -15,42 +15,60 @@ var ptx_lunr_docs = [
   "url": "sec-day1.html",
   "type": "Section",
   "number": "1.1",
-  "title": "",
-  "body": "  Vectors, examples and definition  Linear algebra studies vectors, vector spaces and linear transformations. However, the vectors we study are a generalization of the notion of \"vector\" you have met in your physics and multivariable calculus classes. We have 15 weeks to go from 2D and 3D Euclidean vector spaces to the abstract notion of a vector space, and the tools we use to study these.  For now, a few basic examples:   An Euclidean vector is usually described either geometrically or using Cartesian coordinates:   Since we are discussing \"algebra\", we need to know what are valid algebraic operations with vectors. You know these as:  If we a vector and a number we can scale our given vector by the number to get another vector, namely     If we have two vectors, say we can find their sum     Invalid operations with Euclidean vectors:   Vector Product    Vector Dot product    Vector Cross Product      "
+  "title": "Day One Notes",
+  "body": " Day One Notes   Vectors, examples and definition  Linear algebra studies vectors, vector spaces and linear transformations. However, the vectors we study are a generalization of the notion of \"vector\" you have met in your physics and multivariable calculus classes. We have 15 weeks to go from 2D and 3D Euclidean vector spaces to the abstract notion of a vector space, and the tools we use to study these.  For now, a few basic examples:   An Euclidean vector is usually described either geometrically or using Cartesian coordinates:   Since we are discussing \"algebra\", we need to know what are valid algebraic operations with vectors. You know these as:  If we a vector and a number we can scale our given vector by the number to get another vector, namely     If we have two vectors, say we can find their sum     Invalid operations with Euclidean vectors:   Vector Product    Vector Dot product    Vector Cross Product       A vector space is a set of vectors that has reasonable      "
 },
 {
-  "id": "sec-day1-1-4",
+  "id": "sec-day1-2-4",
   "level": "2",
-  "url": "sec-day1.html#sec-day1-1-4",
+  "url": "sec-day1.html#sec-day1-2-4",
   "type": "Example",
   "number": "1.1.1",
   "title": "",
   "body": " An Euclidean vector is usually described either geometrically or using Cartesian coordinates:   "
 },
 {
-  "id": "sec-day1-1-5",
+  "id": "sec-day1-2-5",
   "level": "2",
-  "url": "sec-day1.html#sec-day1-1-5",
+  "url": "sec-day1.html#sec-day1-2-5",
   "type": "Example",
   "number": "1.1.2",
   "title": "",
   "body": " If we a vector and a number we can scale our given vector by the number to get another vector, namely   "
 },
 {
-  "id": "sec-day1-1-6",
+  "id": "sec-day1-2-6",
   "level": "2",
-  "url": "sec-day1.html#sec-day1-1-6",
+  "url": "sec-day1.html#sec-day1-2-6",
   "type": "Example",
   "number": "1.1.3",
   "title": "",
   "body": " If we have two vectors, say we can find their sum   "
 },
 {
+  "id": "def-",
+  "level": "2",
+  "url": "sec-day1.html#def-",
+  "type": "Definition",
+  "number": "1.1.4",
+  "title": "",
+  "body": "  A vector space is a set of vectors that has reasonable    "
+},
+{
+  "id": "sec-day2",
+  "level": "1",
+  "url": "sec-day2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "A Brief Intro to Matrices",
+  "body": " A Brief Intro to Matrices  Consider the matrix     "
+},
+{
   "id": "sec-worksheets",
   "level": "1",
   "url": "sec-worksheets.html",
   "type": "Section",
-  "number": "1.2",
+  "number": "1.3",
   "title": "intro to Matrices",
   "body": " intro to Matrices   Definitions and examples  Consider the following matrices:   and   Elements  matrix element in a Matrix and The Order of a Matrix    Because the matrix has 3 rows and 3 columns we say that the order of the matrix is .    Because the matrix has ...... rows and ....... columns we say that the order of the matrix is .... .    Because the matrix has ..... rows and columns we say that the order of the matrix is ...........    A matrix is called a square matrix if it has the same number of rows and of columns. In our examples, is a square matrix (because it is a matrix), but neither nor are square.     The element is the element in the first row, third column - so for our example we have .    Find the following elements:           = =   =   =   Transpose of a Matrix  For computational purposes sometimes we need to switch the order of a matrix, so instead of working with a matrix we can make computations with a matrix. This operation is called the transpose of a matrix, and it works by transforming the rows of the given matrix into columns for the transpose. Here is an example:   Consider the following matrices:      1  2  1    3  0  1    2  1  0      A,      1  0    1  1    - 1  - 3      , and      1  0  1    2  1  - 1      C.  Compute the following operations, if possible. If not possible, explain why not:   $A+B$   Not possible because the dimensions of matrices $A$ and $B$ are different.     $3A^T$        3  9  6    6  0  3    3  3  0          $2B+3C$   Not possible because the dimensions of matrices $B$ and $C$ are different.     $B-2C^T$   Not possible because the dimensions of matrices $B$ and $C^T$ are different.     "
 },
@@ -86,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-worksheets.html#sec-worksheets-3",
   "type": "Checkpoint",
-  "number": "1.2.1",
+  "number": "1.3.1",
   "title": "",
   "body": "Consider the following matrices:      1  2  1    3  0  1    2  1  0      A,      1  0    1  1    - 1  - 3      , and      1  0  1    2  1  - 1      C.  Compute the following operations, if possible. If not possible, explain why not:   $A+B$   Not possible because the dimensions of matrices $A$ and $B$ are different.     $3A^T$        3  9  6    6  0  3    3  3  0          $2B+3C$   Not possible because the dimensions of matrices $B$ and $C$ are different.     $B-2C^T$   Not possible because the dimensions of matrices $B$ and $C^T$ are different.    "
 },
@@ -95,18 +113,9 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-section-name.html",
   "type": "Section",
-  "number": "1.3",
+  "number": "1.4",
   "title": "Matrices-Definition New",
   "body": " Matrices-Definition New  Consider the following matrices:    "
-},
-{
-  "id": "sec-secname2",
-  "level": "1",
-  "url": "sec-secname2.html",
-  "type": "Section",
-  "number": "1.4",
-  "title": "Matrix Adition",
-  "body": " Matrix Adition  lets puts something  "
 },
 {
   "id": "backmatter-2",
